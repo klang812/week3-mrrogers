5344c3cf3f22e3eb9126225f1f1b551437bab68f
Kerry Lang <klang812@gmail.com>
2021-01-22 09:09:02 -0800

    see projectissues.txt

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready.function(){
+  $("input").submit(function(event)
+  event.preventDefault();
+  ($("input#userInput").val()
+  alert("BeepBoop!");
+});
+
+function sorry(countTo) {
+  newUserInput;
+  for (let i=0; i<=countTo; i++) {
+  newUserInput+=i;
+  }
+}
