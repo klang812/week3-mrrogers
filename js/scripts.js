cb66bebd7dbcced10a9f62bfa60c5debaceff994
Kerry Lang <klang812@gmail.com>
2021-01-22 11:12:18 -0800

    please see project.txt

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+  event.preventDefault();
+  countTo(parseInt($("input#userInput").val()))
+  $("#output").text(input);
+  });
+});
+
 function countTo(count) {
-  let input=[];
+  let input=0;
   for (let i=0; i<=count; i+=1) {
-  input+=i;
+    input+=i;
   }
 }
 
-$(document).ready.function() {
-  $("#input").submit(function(event) {
-  event.preventDefault();
-  countTo(parseInt($("input#userInput").val()))
-  alert("Success!")
-  });
-});
+
 
