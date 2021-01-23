b987c33b0d51d9b0a40c8cc100979833d7899faf
Kerry Lang <klang812@gmail.com>
2021-01-23 12:23:52 -0800

    test 2 change 3 to string

--- mr-rogers-week3-project/README.md
+++ mr-rogers-week3-project/README.md
@@ -3,13 +3,13 @@ specs
   Test:  "create an array to count 0-user input"
   Expect(countTo(count).toEqual([0-count]);
 
+2. Describe: countTo(count)
+   Test: "replace any numbers containing the integer 3 with string "Won't you be my neighbor"
+   Expect(if #3.toEqual("Won't you be my neighbor?"));
 
 
 
 
-
-
-2.if #3.toEqual("Won't you be my neighbor?")
 3.else if #2.toEqual("Boop!")
 4.else if #1.toEqual("Beep!")
 5. else.toEqual("Don't worry we can still be friends!")
--- mr-rogers-week3-project/js/scripts.js
+++ mr-rogers-week3-project/js/scripts.js
@@ -8,10 +8,36 @@ $(document).ready(function() {
 
 function countTo(count) {
   let number=[];
-  console.log("count: " + count)
-  for (let i=0; i<=count; i++) {
-    console.log("i " + i);
-    number += i;
-  }
+  for (let outerLoopNumber=0; outerLoopNumber<=count; outerLoopNumber++) {
+    let outerLoopNumberAsString = outerLoopNumber.toString()
+    console.log("outer log loop number " + outerLoopNumberAsString);
+    //10 -> "10"
+    //outer log loop number 10
+    let subNumber = "";
+      for (let indexOfOuterLoopNumber=0; indexOfOuterLoopNumber<=outerLoopNumberAsString.length-1; indexOfOuterLoopNumber++) {
+        
+        //if inner loop number is 3 then add Wont you be my neighbor to number
+        // console.log("inner log loop number " + outerLoopNumberAsString[indexOfOuterLoopNumber]);
+        let singleDigit = outerLoopNumberAsString.charAt(indexOfOuterLoopNumber)
+        console.log("inner log loop number " + outerLoopNumberAsString.charAt(indexOfOuterLoopNumber));
+        if (singleDigit === "3") {
+          console.log("single");
+          subNumber = "Won't you be my neighbor?";
+          
+          //update number
+        } 
+        
+      }
+      if (subNumber === "") {
+        number += outerLoopNumberAsString;
+      
+      }
+      else {number+=subNumber}
+      // number += outerLoopNumber;
+    }
+
+
+
+  console.log(number);
   return number;
 }
